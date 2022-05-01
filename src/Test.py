8e3e12c38dc87486c67a7f6a932808f812917a01
Marc-Henry SAINT-FELIX <65421440+msaintfelix@users.noreply.github.com>
2022-05-01 17:53:48 -0400

    Create Test.py

---
+++ src/Test.py
@@ -0,0 +1,6 @@
+# create a pandas dataframe with one column for name and one column for age
+import pandas as pd
+
+df = pd.DataFrame({'Name': ['John', 'Mary', 'Steve', 'Sarah', 'Dave'],
+                   'Age': [21, 22, 23, 24, 25]})
+
